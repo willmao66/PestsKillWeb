40d0e961ce6cf0d30442e7b51a55cac5a1a1f5df
willmao66 <willmao@m3lab.io>
2024-07-06 15:34:34 +0800

    #

--- src/settings.914f1.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"52d36",Atlas:"dee8e",Res:"23ca3",resources:"9544a",main:"b5773"}};
